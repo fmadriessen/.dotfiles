89f59d2fc1e659dfdcdaca1b0bca76b4a7af109b
F. Driessen <fdriessen@posteo.net>
2022-05-05 00:09:48 +0200

    (install) use bash array for proglist Using a bash array allows me to specify the list of programs over multiple lines easier, thus improving readability.

--- install.sh
+++ install.sh
@@ -1,52 +1,72 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 
-# Set some things depending on the OS
-case $(uname) in
-Darwin)
-	hash brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	PKG_MANAGER="brew install"
-	;;
-Linux)
-	case $(grep "^ID=" /etc/os-release | sed 's/^ID=//') in
-	arch) PKG_MANAGER="pkexec pacman -S" ;;
-	*)
-		echo "Unrecognized Linux distribution"
-		exit 1
-		;;
-	esac
-	;;
-*)
-	echo "Unrecognized OS"
-	exit 1
-	;;
-esac
+PROG_LIST=(
+	neovim
+	kitty
+	fish
+
+	# CLI Utils
+	bat
+	ripgrep
+	hyperfine
+	fzf
+	fd
+
+	# LSPs, formatters, and linters
+	bash-language-server
+	shellcheck
+	shfmt
+	shellharden
+
+	lua-language-server
+	selene
+	stylua
+)
 
 link() {
 	echo "Symlinking files into XDG_CONFIG_HOME"
 	export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 	test -d "$XDG_CONFIG_HOME" || mkdir -pv "$XDG_CONFIG_HOME"
 
-	MODULES="nvim kitty bat git"
-	for module in $MODULES; do
-		if [ -e "$XDG_CONFIG_HOME/$module" ]; then
-			echo "$module already exists in $XDG_CONFIG_HOME: not linking $module"
+	for source in xdg_config/*; do
+		dest="$XDG_CONFIG_HOME/${source#*/}"
+		if [ -e "$dest" ]; then
+			echo "$dest already exists: not linking $source"
 			continue
 		fi
 
-		ln -sf "$PWD/xdg_config/$module" "$XDG_CONFIG_HOME/$module"
+		echo "Linking $source to $dest"
+		ln -sf "$PWD/$source" "$dest"
 	done
 }
 
 install() {
 	echo "Installing programs"
-	UTILS="neovim kitty bat ripgrep hyperfine fish fzf fd"
-	LSPS="bash-language-server lua-language-server"
-	"$PKG_MANAGER" "$UTILS" "$LSPS"
+	case $(uname) in
+	Darwin)
+		hash brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+		brew install "${PROG_LIST[@]}"
+		;;
+	Linux)
+		case $(grep "^ID=" /etc/os-release | sed 's/^ID=//') in
+		arch) pkexec pacman -S "${PROG_LIST[@]}" ;;
+		*)
+			echo "Unrecognized Linux distribution"
+			exit 1
+			;;
+		esac
+		;;
+	*)
+		echo "Unrecognized OS"
+		exit 1
+		;;
+	esac
 }
 
 # Argument handling
 if [ $# -lt 1 ]; then
 	link
+	echo
 	install
 else
 	case $1 in
