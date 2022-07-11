6a0bf90d976607ae1ce069711da1bb875a15ba98
F. Driessen <fdriessen@posteo.net>
2022-07-12 00:29:02 +0200

    (feat) nvim: gf in lua

--- xdg_config/nvim/after/ftplugin/lua.lua
+++ xdg_config/nvim/after/ftplugin/lua.lua
@@ -2,3 +2,31 @@
 vim.opt_local.tabstop = 3
 vim.opt_local.shiftwidth = 0
 vim.opt_local.textwidth = 120
+
+---Helper function to find the right file for gf in lua files
+---Width first traversal combination of 'path' and 'suffixesadd'
+---@param fname string
+---@return string
+_G._lua_includeexpr = function(fname)
+   fname = string.gsub(fname, "%.", "/")
+
+   local paths = vim.split(vim.bo.path, ",")
+   local exts = vim.split(vim.bo.suffixesadd, ",")
+
+   for _, path in ipairs(paths) do
+      path = string.match(path, '/$') and path or path .. '/'
+      for _, ext in ipairs(exts) do
+         local f = string.format("%s%s", fname, ext)
+         if vim.fn.filereadable(path .. f) == 1 then
+            return f
+         end
+      end
+   end
+
+   return fname
+end
+
+-- Fix gf for neovim configuration
+vim.opt_local.suffixesadd:prepend({ "/init.lua", ".lua" })
+vim.opt_local.path:prepend({ ".", "lua/" })
+vim.opt_local.includeexpr = "v:lua._lua_includeexpr(v:fname)"
